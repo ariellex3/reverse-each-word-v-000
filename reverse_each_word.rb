53fee55fe6173093bb06361fcd29145a34928ad0
Arielle D <domingu4@gmail.com>
2018-09-11 01:17:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,3 @@ def reverse_each_word(sentence1)
   sentence_array = sentence1.split(/ /)
   sentence_array.each {|word| print word.reverse.join}
 end
-
-
-s = "order. wrong the in are words These"
-	s.split(/(\s+)/). 
-reverse!.join('')   # => "These words are in the wrong order."
-	s.split(/\b/).reverse!.join('')      # => "These words are in the wrong. order"
