8e0b229fccb2f3f69cfc1da53ea7e1b60dbc916a
Arielle D <domingu4@gmail.com>
2018-09-11 01:17:41 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(sentence1)
   sentence_array = sentence1.split(/ /)
-  sentence_array.each {|word| print word.reverse.join}
+  sentence_array.each {|word| word.reverse}
+  word.join
 end
